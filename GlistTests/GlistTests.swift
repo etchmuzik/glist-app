0e31f1eefc635a65479e7fc862b756dae835a551
etch <hesham@beyondmngmt.com>
2025-10-27 03:06:36 +0400

    Initial Commit

---
+++ GlistTests/GlistTests.swift
@@ -0,0 +1,16 @@
+//
+//  GlistTests.swift
+//  GlistTests
+//
+//  Created by ETCH on 27/10/2025.
+//
+
+import Testing
+
+struct GlistTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
